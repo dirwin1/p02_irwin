5d1867710183c6bd8556bb072d888baebb916cd7
Dylan Irwin <dirwin4567@gmail.com>
2019-09-11 12:02:04 -0400

    Initial Commit

---
+++ p02_dirwin1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  p02_dirwin1
+//
+//  Created by Dylan Irwin on 9/11/19.
+//  Copyright © 2019 Dylan Irwin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
